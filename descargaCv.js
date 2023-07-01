85460448070681bad691a8fd288444063a7ae561
Crisleop21 <leonelporoj@gmail.com>
2023-06-30 19:11:26 -0600

    Agregando cv

--- descargaCv.js
+++ descargaCv.js
@@ -1,11 +1,11 @@
 document.getElementById('btnDescargar').addEventListener('click', function() {
     // Ruta del archivo PDF que deseas descargar
-    var pdfUrl = 'cv/CV-Cristian-Poroj-Frontend-Jr.pdf';
+    var pdfUrl = 'cv/CV-Cristian Poroj-Frontend-Jr.pdf';
     
     // Crea un enlace temporal
     var link = document.createElement('a');
     link.href = pdfUrl;
-    link.download = 'CV-Cristian Poroj-Frontend'; // Nombre que se le dará al archivo descargado
+    link.download = 'CV-Cristian-Poroj-Frontend'; // Nombre que se le dará al archivo descargado
     
     // Simula el clic en el enlace
     link.click();
